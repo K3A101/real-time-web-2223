83d120539e668f7eac48d8b7d0d5af493478de9e
K3A101 <keisha.alexander@hva.nl>
2023-04-23 00:03:22 +0200

    dictionary APi geprobeerd

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,2 +1,35 @@
 console.log('script.js loaded');
 const socket = io();
+
+const wordInput = document.querySelector('#word-input');
+const submitButton = document.querySelector('#submit-button');
+console.log(wordInput);
+
+
+wordInput.addEventListener('input', (e) => {
+    e.preventDefault();
+    console.log(wordInput.value);
+})
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const word = wordInput.value;
+
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            fetchData(data)
+        }).catch(err => {
+            console.log(err)
+        })
+})   
+
+
+function fetchData(data) {
+    const definition = data[0].meanings[0].definitions[0].definition;
+
+    const pElement = document.createElement('p');
+    pElement.textContent = definition;
+    document.body.appendChild(pElement);
+}
