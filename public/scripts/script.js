cda38844ee7c758ddffc2272ab7ebd2f38fc6dda
K3A101 <keisha.alexander@hva.nl>
2023-05-08 20:04:39 +0200

    javascript gestructureerd

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,25 +1,35 @@
 console.log('script.js loaded');
 const socket = io();
+// ------------------- VARIABELEN -------------------
 
-const wordInput = document.querySelector('#word-input');
+// Varaiabel voor de Chat onderdelen
 const sendMessage = document.querySelector('#submit-button');
 const chatContainer = document.querySelector('#chat-messages');
-const chatPage = document.querySelector('#words-container');
+const wordsDictionarySection = document.querySelector('#words-container');
+const typingIndicator = document.querySelector('.feedback');
+const chat = document.querySelector('.chat');
+const wordInput = document.querySelector('#word-input');
+const backBtn = document.querySelector('#back-btn');
+
+// Variabel voor het maken van een gebruikersnaam
 const createUserBtn = document.querySelector('#create-user-btn');
 const usernameForm = document.querySelector('.username-form');
 const userList = document.querySelector('#user-online');
 const usernameInput = document.querySelector('#username-input');
-const typingIndicator = document.querySelector('.feedback');
-const chat = document.querySelector('.chat');
-const backBtn = document.querySelector('#back-btn');
 const userListButton = document.querySelector('.user-btn');
+
+
 let currentUser;
 socket.emit('get online users')
 
-// typingIndicator.classList.add('hidden');
 chat.classList.add('hidden');
 backBtn.classList.add('hide-back-btn');
 
+
+// ------------------- EVENTLISTENERS -------------------
+
+// Wanneer de userlist button is geklikt wordt de class 
+//'show-userlist' toegevoegd aan de userlist container
 userListButton.addEventListener('click', (e, button) => {
     e.preventDefault();
     const userlistContainer = document.querySelector('.user-container');
@@ -27,12 +37,11 @@ userListButton.addEventListener('click', (e, button) => {
     // button.classList.toggle('show-userlist')
 });
 
-
-
+// wanneer een gebruikersnaam aangemaakt, word de twee events  naar de server gestuurd
 createUserBtn.addEventListener('click', (e) => {
     e.preventDefault();
     backBtn.classList.remove('hide-back-btn');
-    const usernameInput = document.querySelector('#username-input');
+    // const usernameInput = document.querySelector('#username-input');
     const username = usernameInput.value.trim();
     if (username.length > 0) {
         socket.emit('new-user', username);
@@ -46,6 +55,7 @@ createUserBtn.addEventListener('click', (e) => {
 
 });
 
+//Als je op de back button klikt, ga je terug naar de home pagina met de username formulier
 backBtn.addEventListener('click', (e) => {
     e.preventDefault();
     usernameForm.classList.remove('hidden');
@@ -56,17 +66,16 @@ backBtn.addEventListener('click', (e) => {
 })
 
 
-
 wordInput.addEventListener('input', (e) => {
     let input = wordInput.value;
-    // typingIndicator.classList.add('show-typing-indicator');
     e.preventDefault();
     const typingUser = currentUser;
     socket.emit('typing', typingUser);
-    // console.log(wordInput.value);
 })
 
-
+// Wanneer de gebruiker op het verstuur knop klik, wordt waarde van de input gestuurd naar de API en komt de data terug
+// Verder word de  chat message event gestuurd naar de server
+// De stop typing event wordt gestuurd naar de server
 sendMessage.addEventListener('click', (e) => {
 
     const typingUser = currentUser;
@@ -77,7 +86,7 @@ sendMessage.addEventListener('click', (e) => {
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            socket.emit('wordData', data);
+            socket.emit('wordData', data); //DE data opgehaald van de API wordt gestuurd naar de server
         }).catch(err => {
             console.log(err)
         })
@@ -95,35 +104,24 @@ sendMessage.addEventListener('click', (e) => {
         socket.emit('stop typing', typingUser);
         wordInput.value = '';
     }
-
-
-
 })
 
-socket.on('wordData', (data) => {
-    console.log(data);
-    displayData(data)
-    data.scrollTop = data.scrollHeight;
+// ------------------- DATA UIT API ZICHTBAAR NAAR DE HTML -------------------
 
-})
-
+// Wanneer de gebruiker  wordt de data uit de api opgehaald en wordt de data in de html geplaatst
 function displayData(data) {
-
-
-
     const randomIndex = Math.floor(Math.random() * data[0].meanings[0]?.definitions.length);
     const definition = data[0].meanings[0]?.definitions[randomIndex].definition || 'No definition available';
     const word = data[0].word;
-    // const audioUrl = data[0].phonetics[0]?.audio || data[0].phonetics[1]?.audio||'No audio available';
-    // const phonetics = data[0].phonetics[0]?.text || data[0].phonetic || 'No phonetics available';
-    let audioUrl = 'No audio available';
+    let audioUrl = 'No audio available'; // Als er geen audio beschikbaar is, wordt de audioUrl op 'No audio available' gezet
+
     for (let i = 0; i < data[0].phonetics.length; i++) {
         if (data[0].phonetics[i].audio) {
             audioUrl = data[0].phonetics[i].audio;
             break;
         }
     }
-    // If there is no audio available, the audioUrl will be set to 'No audio available' else it will pick an value from the array
+
     let phonetics = 'No phonetics available'
     for (let t = 0; t < data[0].phonetics.length; t++) {
         if (data[0].phonetics[t].text) {
@@ -132,34 +130,34 @@ function displayData(data) {
         }
     }
 
-
-
     let html = '';
 
     html = `
     <li class="word">
-    <h2>${word}</h2>
+         <h2>${word}</h2>
          <p>${phonetics}</p>  
-         <audio
+        <audio
          controls
-        <source src="${audioUrl}" type="mp3/ogg">
-        
-            
+            <source src="${audioUrl}" type="mp3/ogg">
         </audio>
        <figcaption>${audioUrl}</figcaption>
-       
        <p>${definition}</p>
        <button class="copy-word-btn">Copy the description</button>
-       </li>`
+    </li>`
 
-    chatPage.insertAdjacentHTML('beforeend', html);
-
-
+    wordsDictionarySection.insertAdjacentHTML('beforeend', html);
 
 }
 
+// ----------------- SOCKET REALTIME EVENTS ----------------- //
+// wanneer de server de wordData event stuurt, wordt de data in de displayData functie geplaatst
+socket.on('wordData', (data) => {
+    console.log(data);
+    displayData(data)
+    data.scrollTop = data.scrollHeight;
 
-
+})
+//Hier word de berichten in de chat zichtbaar gemaakt
 socket.on('chat message', (chat) => {
 
     const speechBubble = document.createElement('li');
@@ -172,14 +170,16 @@ socket.on('chat message', (chat) => {
 
     console.log('chat message received')
     //Als de username van de chat message gelijk is aan de username van de input dan wordt de class 'own-message' toegevoegd
+    //Deze class zorgt ervoor dat de speech bubble aan de rechterkant van het scherm wordt geplaatst
     if (chat.username === usernameInput.value) {
         speechBubble.classList.add('own-message');
     }
 
-    // In your own perspectief staat de chat message in de rechterkant van de chat
+    
 
 })
 
+// Wanneer een nieuwe gebruiker wordt aangemaakt, wordt de username in de userList geplaatst
 socket.on('new-user', (username) => {
     console.log(username + ' has joined the chat');
     let user = document.createElement('li');
@@ -187,6 +187,7 @@ socket.on('new-user', (username) => {
     userList.appendChild(user);
 });
 
+// Er komt een bericht binnen dat een gebruiker  in de chat is gekomen
 socket.on('user joined', (username) => {
     console.log(username + ' has joined the chat');
     let user = document.createElement('li');
@@ -194,6 +195,7 @@ socket.on('user joined', (username) => {
     chatContainer.appendChild(user);
 });
 
+// Met deze event kunnen gebruikers zien wie er online is
 socket.on('get online users', (onlineUsers) => {
     userList.innerHTML = '';
     for (username in onlineUsers) {
@@ -203,22 +205,21 @@ socket.on('get online users', (onlineUsers) => {
     }
 });
 
+// Wanneer een gebruiker aan het typen is, kunnen andere gebruikers zien wie aan het typen is
 socket.on('typing', (typingUser) => {
 
     // const typingIndicator = document.createElement('div');
     typingIndicator.innerHTML = `${typingUser} is typing...`;
     console.log(`${typingUser} is typing...`);
     console.log('User is typing')
-    // typingIndicator.classList.remove('hidden');
-
-    // In your own perspectief staat de chat message in de rechterkant van de chat
+   
 
 })
 
+// Wanneer de typende gebruiker  het bericht verstuurt  wordt de typing indicatie verborgen
 socket.on('stop typing', (typingUser) => {
     typingIndicator.innerHTML = '';
     console.log(`${typingUser} has stopped typing...`);
-
     console.log('User has stopped typing');
 
 })
